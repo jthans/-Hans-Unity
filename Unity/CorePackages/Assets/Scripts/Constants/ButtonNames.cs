01cf555a2bf6f74416cff3f8c3bb416cbb5453cb
jthans <jthans@users.noreply.github.com>
2018-12-09 21:17:22 -0500

    Adding (Moving!) Player Controller, as well as updating the project and solution to .Net Framework 4.7 (Was necessary for consuming my personal Nuget packages.

---
+++ Unity/CorePackages/Assets/Scripts/Constants/ButtonNames.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+///  Constants class that contains all controller/button names that will be used in this library.
+/// </summary>
+public class ButtonNames
+{
+    /// <summary>
+    ///  Backward - Backward
+    /// </summary>
+    public const string Backward = "Backward";
+
+    /// <summary>
+    ///  Forward - Forward
+    /// </summary>
+    public const string Forward = "Forward";
+
+    /// <summary>
+    ///  Left - Left
+    /// </summary>
+    public const string Left = "Left";
+
+    /// <summary>
+    ///  Right - Right
+    /// </summary>
+    public const string Right = "Right";
+}
---
+++ Unity/CorePackages/Assets/Scripts/Enums/Direction.cs
@@ -0,0 +1,20 @@
+﻿namespace Hans.Unity.Enums
+{
+	using System;
+
+	/// <summary>
+	///  Directional Enum, that represents a direction an avatar can, or will be moving.  Various combinations
+	/// 	of these values will give more precise/varied directions.
+	/// </summary>
+	[Flags]
+	public enum Direction
+	{
+		Forwards = 1,
+		
+		Backwards = 2,
+
+		Left = 4,
+
+		Right = 8
+	}
+}
