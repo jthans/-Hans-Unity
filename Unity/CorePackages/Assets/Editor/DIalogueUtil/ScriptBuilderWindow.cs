53c02ba08281e0ba8e935f557ab9f607e0491c7b
Taylor Hans <taylor.hans7@gmail.com>
2020-02-10 22:53:00 -0500

    Adding Folder Tracking

---
+++ Unity/CorePackages/Assets/Editor/DIalogueUtil/ScriptBuilderWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+[ExecuteInEditMode]
+public class ScriptBuilderWindow : EditorWindow
+{
+    [MenuItem("Window/ScriptBuilder")]
+    public static void Init()
+    {
+        EditorWindow.GetWindow(typeof(ScriptBuilderWindow)).Show();
+    }
+}
+
+#endif
