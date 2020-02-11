e52c6ba18b3e7499a7e1f0c887e052b1c27489d5
Taylor Hans <taylor.hans7@gmail.com>
2020-02-10 23:14:31 -0500

    Adding Editor Button to Access ScriptBuilder

---
+++ Unity/CorePackages/Assets/Editor/DIalogueUtil/DialogueManagerEditor.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.DialogueSystem;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+/// <summary>
+///  Custom editor for <see cref="DialogueManager" /> objects, so that we can more directly configure the 
+///     dialogue and it's child scripts.
+/// </summary>
+[ExecuteInEditMode]
+[CustomEditor(typeof(DialogueManager))]
+public class DialogueManagerEditor : Editor
+{
+    #region Unity Methods
+
+    /// <summary>
+    ///  When the inspector is rendered, render our custom editor controls.
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        // If the user selects the "ScriptBuilder" option, open the script management utility.
+        if (GUILayout.Button("ScriptBuilder", EditorStyles.miniButton))
+        {
+            ScriptBuilderWindow.Init();
+        }
+    }
+
+    #endregion
+}
+
+#endif
---
+++ Unity/CorePackages/Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Core;
+using Hans.Logging;
+using Hans.Logging.Interfaces;
+
+namespace Assets.Scripts.DialogueSystem
+{
+    /// <summary>
+    ///  Manager class to maintain script reading/reacting in any scene.
+    /// </summary>
+    public class DialogueManager : Singleton<DialogueManager>
+    {
+        /// <summary>
+        ///  Logger object.
+        /// </summary>
+        private ILogger _log;
+
+        #region Unity Methods
+
+        protected override void Awake()
+        {
+            base.Awake();
+            this._log = LoggerManager.CreateLogger(typeof(DialogueManager));
+        }
+
+        #endregion
+    }
+}
